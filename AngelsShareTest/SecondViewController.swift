190b7c1d5fa20375edf09a2166aaa9a4cb791edb
Kelly <kdickerson149@gmail.com>
2017-04-18 21:21:10 -0400

    Tweaked Google Place Picker, tried adding CLLocationManager

--- AngelsShareTest/SecondViewController.swift
+++ AngelsShareTest/SecondViewController.swift
@@ -9,38 +9,21 @@
 import UIKit
 import GoogleMaps
 import GooglePlacePicker
+import GooglePlaces
 
 class SecondViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     
-    @IBAction func buttonPlacePicker(_ sender: UIButton) {
-        let center = CLLocationCoordinate2D(latitude: 37.788204, longitude: -122.411937)
-        let northEast = CLLocationCoordinate2D(latitude: center.latitude + 0.001, longitude: center.longitude + 0.001)
-        let southWest = CLLocationCoordinate2D(latitude: center.latitude - 0.001, longitude: center.longitude - 0.001)
-        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
-        let config = GMSPlacePickerConfig(viewport: viewport)
-        let placePicker = GMSPlacePicker(config: config)
-        
-        placePicker.pickPlace(callback: {(place, error) -> Void in
-            if let error = error {
-                print("Pick Place error: \(error.localizedDescription)")
-                return
-            }
-            
-            if let place = place {
-                self.nameLabel.text = place.name
-                self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
-                    .joined(separator: "\n")
-            } else {
-                self.nameLabel.text = "No place selected"
-                self.addressLabel.text = ""
-            }
-        })        
-    }
-    override func loadView() {
-        super.viewDidLoad()
+    var placesClient: GMSPlacesClient!
+    let locationManager = CLLocationManager()
+
+    
+//    override func loadView() {
+//        super.viewDidLoad()
+//        
+//        print("did it work?")
         /* Maps:
         // Do any additional setup after loading the view, typically from a nib.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
@@ -77,23 +60,47 @@ class SecondViewController: UIViewController, CLLocationManagerDelegate {
 //        }
         
         
-}
+
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        placesClient = GMSPlacesClient.shared()
         // Do any additional setup after loading the view, typically from a nib.
 //        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
 //        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
     
     }
     
+    @IBAction func getCurrentPlace(_ sender: UIButton) {
+        locationManager.requestWhenInUseAuthorization()
+        placesClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.nameLabel.text = "No current place"
+            self.addressLabel.text = ""
+            
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    self.nameLabel.text = place.name
+                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
+                        .joined(separator: "\n")
+                }
+            }
+        })
+    }
 
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         }
 
 
-    }
+}
+
 
