f12cbc78e8cbaf057a4a9d10fb6deca437d81b4f
Kelly <kdickerson149@gmail.com>
2017-04-19 09:23:30 -0400

    Tweaked google place picker in secondviewcontroller and added platform description to podfile. Needs testing on actual device now

--- AngelsShareTest/SecondViewController.swift
+++ AngelsShareTest/SecondViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 import GoogleMaps
 import GooglePlacePicker
 import GooglePlaces
+import CoreLocation
 
 class SecondViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var placePicker: GMSPlacePicker!
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     
     var placesClient: GMSPlacesClient!
-    let locationManager = CLLocationManager()
+    var locationManager = CLLocationManager()
 
     
 //    override func loadView() {
@@ -65,6 +68,10 @@ class SecondViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.startUpdatingLocation()
         placesClient = GMSPlacesClient.shared()
         // Do any additional setup after loading the view, typically from a nib.
 //        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
@@ -73,24 +80,23 @@ class SecondViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func getCurrentPlace(_ sender: UIButton) {
-        locationManager.requestWhenInUseAuthorization()
-        placesClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
+        let config = GMSPlacePickerConfig(viewport: nil)
+        let placePicker = GMSPlacePicker(config: config)
+        
+        placePicker.pickPlace(callback: { (place, error) -> Void in
             if let error = error {
                 print("Pick Place error: \(error.localizedDescription)")
                 return
             }
             
-            self.nameLabel.text = "No current place"
-            self.addressLabel.text = ""
-            
-            if let placeLikelihoodList = placeLikelihoodList {
-                let place = placeLikelihoodList.likelihoods.first?.place
-                if let place = place {
-                    self.nameLabel.text = place.name
-                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
-                        .joined(separator: "\n")
-                }
+            guard let place = place else {
+                print("No place selected")
+                return
             }
+            
+            print("Place name \(place.name)")
+            print("Place address \(place.formattedAddress ?? <#default value#>)")
+            print("Place attributions \(place.attributions ?? <#default value#>)")
         })
     }
 
--- Podfile
+++ Podfile
@@ -1,4 +1,7 @@
 source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
 target 'AngelsShareTest' do
   pod 'GooglePlaces'
   pod 'GooglePlacePicker'
