d3638a34bac4c0fa0a109fa6125001d01a74e1fc
dickersonk <kelly.dickerson@wsj.com>
2017-04-26 14:23:00 -0400

    Added WhiskeyItem struct

---
+++ AngelsShareTest/WhiskeyItem.swift
@@ -0,0 +1,46 @@
+//
+//  WhiskeyItem.swift
+//  AngelsShareTest
+//
+//  Created by Kelly Dickerson on 4/26/17.
+//  Copyright © 2017 Kelly Dickerson. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+//A struct for defining the type of item a user can add to Firebase
+
+struct WhiskeyItem {
+    
+    let key: String
+    let name: String
+    let addedByUser: String
+    let ref: FIRDatabaseReference?
+    var completed: Bool
+    
+    init(name: String, addedByUser: String, completed: Bool, key: String = "") {
+        self.key = key
+        self.name = name
+        self.addedByUser = addedByUser
+        self.completed = completed
+        self.ref = nil
+    }
+    
+    init(snapshot: FIRDataSnapshot) {
+        key = snapshot.key
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        name = snapshotValue["name"] as! String
+        addedByUser = snapshotValue["addedByUser"] as! String
+        completed = snapshotValue["completed"] as! Bool
+        ref = snapshot.ref
+    }
+    
+    func toAnyObject() -> Any {
+        return [
+            "name": name,
+            "addedByUser": addedByUser,
+            "completed": completed
+        ]
+    }
+}
