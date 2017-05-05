486a4d477d09f9afdb9ec14853c92e6e47f296e3
dickersonk <kelly.dickerson@wsj.com>
2017-05-05 17:36:52 -0400

    Created a add new whiskey button on first view controller

---
+++ AngelsShareTest/AddNewViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddNewViewController.swift
+//  AngelsShareTest
+//
+//  Created by Kelly Dickerson on 5/5/17.
+//  Copyright © 2017 Kelly Dickerson. All rights reserved.
+//
+
+import UIKit
+
+class AddNewViewController: UIViewController {
+
+    @IBOutlet weak var newWhiskey: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        
+    }
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
